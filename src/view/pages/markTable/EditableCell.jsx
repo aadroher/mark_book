e70bcffb224c2d8dafdbd5bcf085819e986c2c4a
Armand Adroher <aadroher@gmail.com>
2018-02-13 23:42:08 +0000

    First steps by using a class component.

--- src/view/pages/markTable/EditableCell.jsx
+++ src/view/pages/markTable/EditableCell.jsx
@@ -1,6 +1,40 @@
 import React from 'react'
 import styles from './EditableCell.module.css'
 
+class StudentCellInput extends React.Component {
+  constructor(props) {
+    super(props)
+
+    console.log(props)
+
+    const {value} = props
+    this.state = {value}
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleBlur = this.handleBlur.bind(this)
+  }
+
+  handleChange(event) {
+    const {value} = event.target
+    this.setState({value})
+  }
+
+  handleBlur(event) {
+    console.log('lost focus')
+  }
+
+  render() {
+    const {value} = this.state
+    return (
+      <input
+        type='text'
+        value={value}
+        onChange={this.handleChange}
+      />
+    )
+  }
+
+}
 
 const EditableCell = ({cell, onStudentCellClick}) =>
   <td
@@ -12,7 +46,7 @@ const EditableCell = ({cell, onStudentCellClick}) =>
   >
     {
       cell.isInEditionMode
-        ? <input type='text' value={cell.value} onChange={(e) => {console.log(e.target)}}/>
+        ? <StudentCellInput value={cell.value}/>
         : cell.value
     }
   </td>
